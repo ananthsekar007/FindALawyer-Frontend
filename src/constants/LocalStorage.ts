56bb4fc79b6197f2a01d98bb7e7cf059f59909b7
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-02 18:08:18 +0530

    Added localstorage functions for client and lawyer

---
+++ src/constants/LocalStorage.ts
@@ -0,0 +1,54 @@
+
+const FindALawyerClient = "FindALawyerClient";
+const FindALawyerLawyer = "FindALawyerLawyer";
+
+const FindALawyerClientToken = "FindALawyerClientToken";
+const FindALawyerLawyerToken = "FindALawyerLawyerToken";
+
+export const setClient = (client : Client) => {
+    localStorage.setItem(FindALawyerClient, JSON.stringify(client));
+}
+
+export const getClient = () : Client | null => {
+    let user = localStorage.getItem(FindALawyerClient);
+    if(!user) return null; 
+    return JSON.parse(user);
+}
+
+export const setClientAuthToken = (token: string) => {
+    localStorage.setItem(FindALawyerClientToken, token);
+}
+
+export const getClientAuthToken = (): string | null => {
+    return localStorage.getItem(FindALawyerClientToken);
+}
+
+// export const setLawyer = (lawyer : Resident) => {
+//     localStorage.setItem(FindALawyerLawyer, JSON.stringify(lawyer));
+// }
+
+// export const getLawyer = () : Resident | null => {
+//     let user = localStorage.getItem(FindALawyerLawyer);
+//     if(!user) return null; 
+//     return JSON.parse(user);
+// }
+
+export const setLawyerAuthToken = (token: string) => {
+    localStorage.setItem(FindALawyerLawyerToken, token);
+}
+
+export const getLawyerAuthToken = (): string | null => {
+    return localStorage.getItem(FindALawyerLawyerToken);
+}
+
+
+export const clientLogout = () => {
+    localStorage.removeItem(FindALawyerClient);
+    localStorage.removeItem(FindALawyerClientToken);
+}
+
+
+export const lawyerLogout = () => {
+    localStorage.removeItem(FindALawyerLawyer);
+    localStorage.removeItem(FindALawyerLawyerToken);
+}
