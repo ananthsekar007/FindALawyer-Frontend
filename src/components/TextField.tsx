88a3ad7503d9e58cc919183b010b923e7f547e73
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-02 17:54:21 +0530

    Modified TextField component with validation using react hook form

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { RegisterOptions } from "react-hook-form";
 
 interface TextFieldProps
   extends Partial<React.InputHTMLAttributes<HTMLInputElement>> {
-  label: string;
-  id: string | undefined;
   name: string;
+  label: string;
+  register: any;
+  errors?: any;
+  validationSchema?: RegisterOptions;
 }
 
-const Textfield = ({ id, label, name, ...rest }: TextFieldProps) => {
+const TextField = ({ id, label, name, errors, register, validationSchema, ...rest }: TextFieldProps) => {
   return (
     <div>
       <label
@@ -18,14 +21,20 @@ const Textfield = ({ id, label, name, ...rest }: TextFieldProps) => {
       </label>
       <div className="mt-2">
         <input
-          id={id}
+          id={name}
           name={name}
+          {...register(name, validationSchema)}
           {...rest}
           className="p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
       </div>
+      {errors[name] && (
+        <span className="text-xs text-red-500 font-semibold">
+          {errors[name].message}
+        </span>
+      )}
     </div>
   );
 };
 
-export default Textfield;
+export default TextField;
