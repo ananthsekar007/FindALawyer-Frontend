4c2cb37125b3221c74406621287f8a43b3964ab9
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-05 10:36:08 +0530

    modified interceptors for botn client and lawyers

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../constants/AppConstants';
+import { getClientAuthToken, getLawyerAuthToken } from '../constants/LocalStorage';
 // import { getToken } from '../hooks/useToken';
 
-const axiosInstance = axios.create({
+const axiosClientInstance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-axiosInstance.interceptors.request.use(
+const axiosLawyerInstance = axios.create({
+  baseURL: API_BASE_URL
+})
+
+axiosClientInstance.interceptors.request.use(
   (config) => {
-    const token = "getToken()";
+    const token = getClientAuthToken();
     const auth = token ? `Bearer ${token}` : '';
     config.headers.common['Authorization'] = auth;
     return config;
@@ -16,4 +21,14 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
-export default axiosInstance;
+axiosLawyerInstance.interceptors.request.use(
+  (config) => {
+    const token = getLawyerAuthToken();
+    const auth = token ? `Bearer ${token}` : '';
+    config.headers.common['Authorization'] = auth;
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+export {axiosClientInstance, axiosLawyerInstance};
