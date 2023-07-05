6fd6bee842ea2bb10effaedcb971d401d237e459
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-05 18:02:23 +0530

    Added multiline text field component

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { RegisterOptions } from "react-hook-form";
 
 interface TextFieldProps
-  extends Partial<React.InputHTMLAttributes<HTMLInputElement>> {
+  extends Partial<React.InputHTMLAttributes<HTMLInputElement>>  {
   name: string;
   label: string;
   register: any;
   errors?: any;
   validationSchema?: RegisterOptions;
+  multiline?: boolean; 
 }
 
-const TextField = ({ id, label, name, errors, register, validationSchema, ...rest }: TextFieldProps) => {
+const TextField = ({ id, label, name, errors, register, validationSchema, multiline, ...rest }: TextFieldProps) => {
+
   return (
     <div>
       <label
@@ -20,13 +22,23 @@ const TextField = ({ id, label, name, errors, register, validationSchema, ...res
         {label}
       </label>
       <div className="mt-2">
-        <input
-          id={name}
-          name={name}
-          {...register(name, validationSchema)}
-          {...rest}
-          className="p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
+        {multiline ? (
+          <textarea
+            id={name}
+            name={name}
+            {...register(name, validationSchema)}
+            {...rest}
+            className="p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        ) : (
+          <input
+            id={name}
+            name={name}
+            {...register(name, validationSchema)}
+            {...rest}
+            className="p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        )}
       </div>
       {errors[name] && (
         <span className="text-xs text-red-500 font-semibold">
