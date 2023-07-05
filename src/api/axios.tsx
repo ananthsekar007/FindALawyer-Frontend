4a3f23215f0a7e9a29868371b6253635793ba926
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-05 10:56:15 +0530

    Fixed an error while setting auth token to header

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,6 +1,9 @@
-import axios from 'axios';
-import { API_BASE_URL } from '../constants/AppConstants';
-import { getClientAuthToken, getLawyerAuthToken } from '../constants/LocalStorage';
+import axios from "axios";
+import { API_BASE_URL } from "../constants/AppConstants";
+import {
+  getClientAuthToken,
+  getLawyerAuthToken,
+} from "../constants/LocalStorage";
 // import { getToken } from '../hooks/useToken';
 
 const axiosClientInstance = axios.create({
@@ -8,27 +11,27 @@ const axiosClientInstance = axios.create({
 });
 
 const axiosLawyerInstance = axios.create({
-  baseURL: API_BASE_URL
-})
+  baseURL: API_BASE_URL,
+});
 
 axiosClientInstance.interceptors.request.use(
   (config) => {
     const token = getClientAuthToken();
-    const auth = token ? `Bearer ${token}` : '';
-    config.headers.common['Authorization'] = auth;
+    const auth = token ? `Bearer ${token}` : "";
+    config.headers["Authorization"] = auth;
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
 axiosLawyerInstance.interceptors.request.use(
   (config) => {
     const token = getLawyerAuthToken();
-    const auth = token ? `Bearer ${token}` : '';
-    config.headers.common['Authorization'] = auth;
+    const auth = token ? `Bearer ${token}` : "";
+    config.headers["Authorization"] = auth;
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
-export {axiosClientInstance, axiosLawyerInstance};
+export { axiosClientInstance, axiosLawyerInstance };
