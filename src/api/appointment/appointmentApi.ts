26cf9ad0513860ac626e8650acdd0f4050967566
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-10 12:30:17 +0530

    Added API for updating the appointments for client and lawyer

--- src/api/appointment/appointmentApi.ts
+++ src/api/appointment/appointmentApi.ts
@@ -1,14 +1,38 @@
 import { BookingFormData } from "../../components/appointment/BookAppointmentModal";
-import { axiosClientInstance } from "../axios";
+import { axiosClientInstance, axiosLawyerInstance } from "../axios";
 
 export const bookAppointment = (data: BookingFormData) => {
-    return axiosClientInstance.post("/appointment/book", data);
-}
+  return axiosClientInstance.post("/appointment/book", data);
+};
 
 export const getClientAppointments = (clientId: number, status: string) => {
-    return axiosClientInstance.get(`/appointment/client/get?clientId=${clientId}&status=${status}`);
-}
+  return axiosClientInstance.get(
+    `/appointment/client/get?clientId=${clientId}&status=${status}`
+  );
+};
 
 export const getLawyerAppointments = (lawyerId: number, status: string) => {
-    return axiosClientInstance.get(`/appointment/lawyers/get?lawyerId=${lawyerId}&status=${status}`);
-}
+  return axiosClientInstance.get(
+    `/appointment/lawyers/get?lawyerId=${lawyerId}&status=${status}`
+  );
+};
+
+export const updateAppointmentsForClient = (
+  appointmentId: number,
+  status: string
+) => {
+  return axiosClientInstance.put("/appointment/update", {
+    appointmentId,
+    status,
+  });
+};
+
+export const updateAppointmentsForLawyer = (
+    appointmentId: number,
+    status: string
+  ) => {
+    return axiosLawyerInstance.put("/appointment/update", {
+      appointmentId,
+      status,
+    });
+  };
