e60315c89eb242d5ec3d1de5c8579d301b1d8dc3
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-10 14:44:44 +0530

    Added loader functionality to outlined button

--- src/components/OutlinedButton.tsx
+++ src/components/OutlinedButton.tsx
@@ -14,10 +14,15 @@ const OutlinedButton = ({
 }: OutlinedButtonProps) => {
   return (
     <button
-      className={`w-full bg-white hover:bg-transparent hover:border-blue-500 hover:text-blue-500 border border-blue-500 hover:scale-105 rounded-lg p-2 text-blue-500 font-semibold ${className}`}
+      className={`w-full bg-white disabled:hover:cursor-not-allowed hover:bg-transparent hover:border-blue-500 hover:text-blue-500 border border-blue-500 hover:scale-105 rounded-lg p-2 text-blue-500 font-semibold ${className}`}
       {...rest}
+      disabled={loading}
     >
-      {text}
+      {loading ? (
+        <i className="fas fa-spinner fa-spin text-blue-500 text-xl"></i>
+      ) : (
+        text
+      )}
     </button>
   );
 };
