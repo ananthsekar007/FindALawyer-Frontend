596974a6fd6e0e22c7f5fd9acd6b65ea825825d5
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-10 21:31:45 +0530

    Implemented home page for lawyer

--- src/pages/lawyer/Home.tsx
+++ src/pages/lawyer/Home.tsx
@@ -1,18 +1,29 @@
 import LawyerLayout from "../../components/lawyer/LawyerLayout";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Appointment } from "../../types/AppointmentType";
 import { getLawyer } from "../../constants/LocalStorage";
 import { getLawyerAppointments } from "../../api/appointment/appointmentApi";
 import { AppointmentTypes } from "../../constants/AppConstants";
-import Button from "../../components/Button";
-import { useNavigate } from "react-router-dom";
+import PendingLawyerAppointments from "../../components/lawyer/PendingLawyerAppointments";
+import Loader from "../../components/Loader";
+import ActiveLawyerAppointments from "../../components/lawyer/ActiveLawyerAppointments";
+import RejectedLawyerAppointments from "../../components/lawyer/RejectedLawyerAppointments";
+import CompletedLawyerAppointments from "../../components/lawyer/CompletedLawyerAppointments";
 
 const LawyerHome = () => {
   const [pendingAppointments, setPendingAppointments] = useState<Appointment[]>(
     []
   );
-
-  const navigate = useNavigate();
+  const [activeAppointments, setActiveAppointments] = useState<Appointment[]>(
+    []
+  );
+  const [rejectedAppointments, setRejectedAppointments] = useState<
+    Appointment[]
+  >([]);
+  const [completedAppointments, setCompletedAppointments] = useState<
+    Appointment[]
+  >([]);
+  const [loading, setloading] = useState<boolean>(true);
 
   const getPendingAppointments = async () => {
     const lawyer = getLawyer();
@@ -24,54 +35,74 @@ const LawyerHome = () => {
     setPendingAppointments(response.data.response);
   };
 
-  useEffect(() => {
-    console.log({ pendingAppointments });
-  }, [pendingAppointments]);
+  const getActiveAppointments = async () => {
+    const lawyer = getLawyer();
+    if (!lawyer) return;
+    const response = await getLawyerAppointments(
+      lawyer.lawyerId,
+      AppointmentTypes.ACTIVE
+    );
+    setActiveAppointments(response.data.response);
+  };
+
+  const getRejectedAppointments = async () => {
+    const lawyer = getLawyer();
+    if (!lawyer) return;
+    const response = await getLawyerAppointments(
+      lawyer.lawyerId,
+      AppointmentTypes.REJECTED
+    );
+    setRejectedAppointments(response.data.response);
+  };
+
+  const getCompletedAppointments = async () => {
+    const client = getLawyer();
+    if (!client) return;
+    const response = await getLawyerAppointments(
+      client.lawyerId,
+      AppointmentTypes.COMPELETED
+    );
+    setCompletedAppointments(response.data.response);
+  };
+
+  const getAllAppointments = async () => {
+    setloading(true);
+    await getPendingAppointments();
+    await getActiveAppointments();
+    await getRejectedAppointments();
+    await getCompletedAppointments();
+    setloading(false);
+  };
 
   useEffect(() => {
-    getPendingAppointments();
+    getAllAppointments();
   }, []);
 
   return (
     <LawyerLayout>
+      {loading && <Loader />}
       <div className="grid grid-cols-1 w-full p-5 gap-3 md:grid-cols-2">
         <div className="w-full h-56 md:h-64 bg-white rounded shadow p-5">
-          <p className="font-semibold text-start">Pending Appointments</p>
-          <div className="w-full h-36 md:h-44 bg-slate-100 rounded mt-3 grid grid-cols-4 text-sm p-3 overflow-y-scroll">
-            <p className="font-semibold">Name</p>
-            <p className="col-span-2 font-semibold ">Description</p>
-            <p className="font-semibold justify-self-center mb-6">Action</p>
-            {pendingAppointments.length > 0 &&
-              pendingAppointments.map((appointment, index) => (
-                <>
-                  <p className="justify-self-start h-12">
-                    {appointment.client.name}
-                  </p>
-                  <p className="col-span-2 h-12">
-                    {appointment.caseDescription}
-                  </p>
-                  <Button
-                    text="Chat"
-                    onClick={() => {
-                      console.log("clicked");
-                      navigate(`/lawyer/chat`, {
-                        state: {
-                            appointment
-                        }
-                      });
-                    }}
-                    className="h-fit w-16 justify-self-center"
-                  />
-                  {/* <div className="bg-yellow-600  text-white rounded-xl h-fit p-1 px-2 justify-self-center">
-                    {appointment.status}
-                  </div> */}
-                </>
-              ))}
-          </div>
+          <PendingLawyerAppointments
+            pendingAppointments={pendingAppointments}
+            onSuccess={() => {
+              getAllAppointments();
+            }}
+          />
         </div>
-        <div className="w-full h-56 md:h-64 bg-white  rounded shadow"></div>
-        <div className="w-full h-56 md:h-64 bg-white rounded shadow"></div>
-        <div className="w-full h-56 md:h-64 bg-white rounded shadow"></div>
+        <div className="w-full h-56 md:h-64 bg-white  rounded shadow p-5">
+          <ActiveLawyerAppointments activeAppointments={activeAppointments} />
+        </div>
+        <div className="w-full h-56 md:h-64 bg-white rounded shadow p-5">
+          <RejectedLawyerAppointments
+            rejectedAppointments={rejectedAppointments}
+          />
+        </div>
+        <div className="w-full h-56 md:h-64 bg-white rounded shadow p-5">
+          <CompletedLawyerAppointments
+            completedAppointments={completedAppointments}
+          />
+        </div>
       </div>
     </LawyerLayout>
   );
