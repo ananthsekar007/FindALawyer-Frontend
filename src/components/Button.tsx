f9070b35ce6330333c30cdf3ec3bc89bcc632ef9
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-10 12:29:54 +0530

    Modified button with loader component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,10 +9,15 @@ interface TextFieldProps
 const Button = ({ loading, text, className, ...rest }: TextFieldProps) => {
   return (
     <button
-      className={`w-full bg-gradient-to-br from-blue-500 to-purple-500 hover:from-purple-500 hover:to-blue-500 hover:scale-105 rounded-lg p-2 text-white ${className}`}
+      className={`w-full bg-gradient-to-br disabled:hover:cursor-not-allowed from-blue-500 to-purple-500 hover:from-purple-500 hover:to-blue-500 hover:scale-105 rounded-lg p-2 text-white ${className}`}
       {...rest}
+      disabled={loading}
     >
-      {text}
+      {loading ? (
+        <i className="fas fa-spinner fa-spin text-white text-xl"></i>
+      ) : (
+        text
+      )}
     </button>
   );
 };
