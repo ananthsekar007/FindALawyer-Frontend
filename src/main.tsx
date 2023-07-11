26e49301c183ee810e5086a9681ddb73505e0385
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-11 22:32:46 +0530

    Added toast functions using react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./App";
 import 'react-responsive-modal/styles.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
