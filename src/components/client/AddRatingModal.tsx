c34be42354568384086b96f195d67a945c044a3c
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-11 21:17:13 +0530

    Added modal ot add rating

---
+++ src/components/client/AddRatingModal.tsx
@@ -0,0 +1,92 @@
+import Modal from "react-responsive-modal";
+import StarRating from "../StarRating";
+import { useState } from "react";
+import TextField from "../TextField";
+import { useForm } from "react-hook-form";
+import Button from "../Button";
+import { RateLawyersBody, rateLawyers } from "../../api/lawyer/lawyerApi";
+import { AxiosError } from "axios";
+
+interface AddRatingModalProps {
+  open: boolean;
+  onClose: () => void;
+  lawyerId?: number;
+  clientId?: number;
+}
+
+interface StarRatingData {
+  remarks: string;
+}
+
+function AddRatingModal(props: AddRatingModalProps) {
+  const [rating, setRating] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<StarRatingData>();
+
+  const onSubmit = async (data: StarRatingData) => {
+    if (!props.lawyerId && !props.clientId) return;
+    const body: RateLawyersBody = {
+      clientId: Number(props.clientId),
+      lawyerId: Number(props.lawyerId),
+      ratingValue: rating,
+      remarks: data.remarks,
+    };
+    setLoading(true);
+    try {
+        const response = await rateLawyers(body);
+        if (!response.data) return;
+    }
+    catch(e : any) {
+        console.log("error", {e: e.response.data});
+    }
+    finally {
+        setLoading(false);
+        props.onClose();
+    }
+  };
+
+  return (
+    <Modal
+      open={props.open}
+      onClose={props.onClose}
+      center
+      classNames={{
+        modal: "w-3/6",
+      }}
+    >
+      <p className="font-semibold text-lg">Rate the lawyer</p>
+      <StarRating
+        isEditable
+        initialRating={0}
+        onChange={(rating) => {
+          setRating(rating);
+        }}
+      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="Give Remarks"
+          name="remarks"
+          type="text"
+          multiline
+          id="remarks"
+          register={register}
+          errors={errors}
+          validationSchema={{
+            required: {
+              value: true,
+              message: "Remarks is required!",
+            },
+          }}
+        />
+        <Button text="Review" type="submit" loading={loading} />
+      </form>
+    </Modal>
+  );
+}
+
+export default AddRatingModal;
