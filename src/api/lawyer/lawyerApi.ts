7086a70c1b5f5aab46584908f078308ab6914874
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-11 21:17:03 +0530

    Added APi for rating the lawyers

--- src/api/lawyer/lawyerApi.ts
+++ src/api/lawyer/lawyerApi.ts
@@ -1,5 +1,16 @@
-import { axiosClientInstance } from "../axios"
+import { axiosClientInstance } from "../axios";
 
 export const listAllLawyers = () => {
-    return axiosClientInstance.get("/lawyer/getall");
+  return axiosClientInstance.get("/lawyer/getall");
+};
+
+export interface RateLawyersBody {
+  lawyerId: number;
+  clientId: number;
+  ratingValue: number;
+  remarks: string;
 }
+
+export const rateLawyers = (data: RateLawyersBody) => {
+  return axiosClientInstance.post("/lawyer/rate", data);
+};
